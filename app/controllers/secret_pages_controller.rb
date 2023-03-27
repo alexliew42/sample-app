dd11c1e55b79097b68258c860ceccbed5c590d85
Alex Liew <liewtwoo42@gmail.com>
2023-03-27 13:19:34 -0400

    add winner action, add secretpage2

--- app/controllers/secret_pages_controller.rb
+++ app/controllers/secret_pages_controller.rb
@@ -2,4 +2,9 @@ class SecretPagesController < ApplicationController
   def printer
     render json: {secret: "This is a secret"}
   end
+
+  def winner
+    render json: {winner: "You Win!"}
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/secret_hash", controller: "secret_pages", action: "printer"
+  get "/secret_1", controller: "secret_pages", action: "printer"
+  get "/secret_2", controller: "secret_pages", action: "winner"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
